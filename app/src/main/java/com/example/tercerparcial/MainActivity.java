80c239a8e4d7e734011aa85a8c8d412a2983f9d4
Rafaelmag3 <rafaelmonrroy045gmail.com>
2023-03-24 12:55:48 -0600

    Navegacíon

---
+++ app/src/main/java/com/example/tercerparcial/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.tercerparcial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    FirstFragment firstfragment = new FirstFragment();
+    SecondFragment secondfragment = new SecondFragment();
+    ThirdFragment thirdfragment = new ThirdFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(firstfragment);
+    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.firtstFragment:
+                    loadFragment(firstfragment);
+                    return true;
+                case R.id.secondFragment:
+                    loadFragment(secondfragment);
+                    return true;
+                case R.id.thirdFragment:
+                    loadFragment(thirdfragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+    public void loadFragment (Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+}
